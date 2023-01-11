6729667556ee31ba8599436e489221393aadb070
DMMutua <lengowriter99@gmail.com>
2023-01-11 11:48:25 +0300

    changing command and path arguments in exec using manifest

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Manifest for Killing a Process named `killmenow`
 exec { 'kill_killmenow_process':
-  command => 'kill -9 $(pgrep killmenow)',
-  path => '/',
+  command => 'pkill -9 killmenow)',
+  path => '/usr/bin:/usr/local/bin',
   onlyif => 'pgrep killmenow',
 }
