afe69a202cf6ccc13bc793c83e3e3376e560f756
DMMutua <lengowriter99@gmail.com>
2023-01-11 11:53:27 +0300

    passing path to executable of pkill to command attribute of exec

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,4 @@
 # Manifest for Killing a Process named `killmenow`
-exec { 'kill_killmenow_process':
-  command => 'pkill -9 killmenow)',
-  path => '/usr/bin:/usr/local/bin',
-  onlyif => 'pgrep killmenow',
+exec { 'killmenow':
+  command => '/usr/bin/pkill killmenow)',
 }
